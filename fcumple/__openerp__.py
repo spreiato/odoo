c24acd7922aae173f64c867453ce797e47ed8aa6
spreiato <santiago.m.preiato@gmail.com>
2020-03-13 12:28:39 -0300

    fc

---
+++ fcumple/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Fecha Cumpleaños Cliente',
+    'version': '1.0',
+    'author': ['Santiago Preiato'],
+    'category': 'Tools',
+    'summary': 'Fecha Cumpleaños Cliente',
+    'website': 'https://www.sgpgestion.com.ar',
+    'description': """
+Fecha Cumpleaños
+======================
+Con este módulo mostraremos Fecha Cumpleaños
+    """,
+    'depends': ['base'],
+    'data': [
+        'preciocostousd_view.xml',
+    ],
+    'installable': True,
+    'auto_install': True,
+}
